cac5366075b3ba32783acef9c9239e1b4a211205
Chris <nothanks@nowhere>
2018-04-22 16:19:16 +0100

    m'lkn
    'lkn 'ln

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,5 @@ var gulp = require('gulp');var googleWebFonts = require('gulp-google-webfonts');
 
 var options = { };
 
-gulp.task('2szfonts', function () {return gulp.src('./2szfonts.list').pipe(googleWebFonts(options)).pipe(gulp.dest('out/fonts'));	});
+gulp.task('2szfonts', function () {return gulp.src('./2szfonts.list').pipe(googleWebFonts(options)).pipe(gulp.dest('docs/fonts'));	});
 gulp.task('default', ['2szfonts']);
